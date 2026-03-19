5da0e4b2a03fb40640b9b1c08c271a3a09e27ba0
Vernon Rauch <vrauch@gmail.com>
2026-03-20 00:56:45 +1100

    Fix start.sh: add -d flag to docker compose and wait for Docker daemon

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 printenv ENV_FILE > .env
-printenv AUTH_CONFIG >auth_config.yaml
-docker compose up --build
+printenv AUTH_CONFIG > auth_config.yaml
+
+# Wait for Docker daemon to be ready (DinD can take a few seconds)
+until docker info > /dev/null 2>&1; do
+  echo "Waiting for Docker daemon..."
+  sleep 2
+done
+
+docker compose up --build -d
